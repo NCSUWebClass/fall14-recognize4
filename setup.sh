a06b038943a6f650c31c173e84eb92d29b8035dd
Miles Richardson <miles.s.richardson@gmail.com>
2014-10-31 23:01:44 -0400

    Fix database initialization in setup script and add colors

--- setup.sh
+++ setup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+COLOR_BLUE="\033[1;34m"
+COLOR_RESET="\033[0m"
+
 # create virtual environment
+echo -e "\n${COLOR_BLUE}[[ Creating Virtual Environment ]]${COLOR_RESET}"
 python3 -m venv .
 
 # install dependencies
+echo -e "\n${COLOR_BLUE}[[ Installing Dependencies ]]${COLOR_RESET}\n"
 ./bin/python3 -m pip install "django==1.6" "django-filer==0.9.7"
 
 # initialize database
-./recognize_backend/manage.py syncdb
+echo -e "\n${COLOR_BLUE}[[ Initializing Database ]]${COLOR_RESET}\n"
+./bin/python3 recognize_backend/manage.py syncdb
