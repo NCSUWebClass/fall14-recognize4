7b09bf3c9ed52886cf1076553bd9018e3e7528e6
Miles Richardson <miles.s.richardson@gmail.com>
2014-11-01 18:53:28 -0400

    Enhance setup script to work with earlier Python 3 versions

--- setup.sh
+++ setup.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
+# Configuration ###############################################################
+
+VIRTUALENV_VERSION="1.11.6"
+VIRTUALENV_URL="https://pypi.python.org/packages/source/v/virtualenv/virtualenv-${VIRTUALENV_VERSION}.tar.gz"
+
 COLOR_BLUE="\033[1;34m"
 COLOR_RESET="\033[0m"
 
-# create virtual environment
-echo -e "\n${COLOR_BLUE}[[ Creating Virtual Environment ]]${COLOR_RESET}"
-python3 -m venv .
+# Main Execution ##############################################################
 
-# install dependencies
+cd `dirname "${0}"`
+
+echo -e "\n${COLOR_BLUE}[[ Fetching Virtual Environment Scripts ]]${COLOR_RESET}"
+
+curl -O "${VIRTUALENV_URL}"
+tar xvzf "virtualenv-${VIRTUALENV_VERSION}.tar.gz"
+mv "virtualenv-${VIRTUALENV_VERSION}" "virtualenv"
+rm -f "virtualenv-${VIRTUALENV_VERSION}.tar.gz"
+
+echo -e "\n${COLOR_BLUE}[[ Creating Virtual Environment ]]${COLOR_RESET}"
+
+./virtualenv/virtualenv.py --python="python3" .
+
 echo -e "\n${COLOR_BLUE}[[ Installing Dependencies ]]${COLOR_RESET}\n"
+
 ./bin/python3 -m pip install "django==1.6" "django-filer==0.9.7"
 
-# initialize database
 echo -e "\n${COLOR_BLUE}[[ Initializing Database ]]${COLOR_RESET}\n"
+
 ./bin/python3 recognize_backend/manage.py syncdb
