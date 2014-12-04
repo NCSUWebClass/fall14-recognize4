7caa43c66d97d31861fa96fed24a766b03abdf02
Miles Richardson <miles@bitbutt.io>
2014-12-04 00:05:08 -0500

    Add new sound effects

--- app/recognize/settings.py
+++ app/recognize/settings.py
@@ -13,7 +13,7 @@ TEMPLATE_DEBUG = True
 ALLOWED_HOSTS = []
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, "templates")]
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Application definition
 
