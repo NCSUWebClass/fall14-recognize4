fec45d2ef71cb083577d6f1bc775f3b58b06b9e4
Miles Richardson <miles@bitbutt.io>
2014-12-07 23:55:53 -0500

    Modify some styles

--- app/recognize/views.py
+++ app/recognize/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from recognize_albums.models import Album, Image
 
 def index(request):
-    return render(request, "game/home.html")
+    albums = []
+
+    for album_entry in Album.objects.all():
+        images = []
+
+        for image_entry in Image.objects.filter(album__title=album_entry.title):
+            images.append(image_entry.image.url)
+
+        albums.append({
+            "title": album_entry.title,
+            "images": images
+        })
+
+    return render(request, "game/home.html", {"albums": albums})
 
 def game(request):
     return render(request, "game/recog.html")
